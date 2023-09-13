c875bd60f9e0d2359860ed56d0e18a73af1e58ab
ievrbga <iev.rb.ga@gmail.com>
2023-09-13 20:12:14 +0300

    gitTest

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,7 +10,7 @@ public class Main {
 
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
+            System.out.println("i = " + i + 1);
         }
     }
 }
